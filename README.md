96d3cc603fb7687ccc2011c0e41821772dc39888
mykhailo-rybnikov <60021787+mykhailo-rybnikov@users.noreply.github.com>
2020-06-18 16:11:18 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-👼Our Father, which art in heaven,👼
-hallowed be thy name;
-thy kingdom come;
-thy will be done in earth as it is in heaven.
-Give us this day our daily bread;
-and forgive us our trespasses
-as we forgive them who trespass against us;
-and lead us not into temptation,
-but deliver us from the evil one.
-For thine is the kingdom
-and the power and the glory, for ever.
-Amen 🙏
+Task-List
--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-let el = document.createElement('li');
-let div = document.getElementById('container');
-let input1 = document.createElement('input');
-let button1 = document.createElement('button');
-let a = 0;
-let del = [];
-let list;
-
-button1.addEventListener('click',OnClick);
-button1.addEventListener('',OnClick);
-document.addEventListener('keypress', (event) => {
-    if(event.key === "Enter"){
-    OnClick();
-    }
-})
-
-button1.innerHTML = "Добавить"
-div.appendChild(input1);
-div.appendChild(button1);
-function OnClick(){
+function restart(){
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    if(tasks===null){tasks = []}
     
-if(input1.value != ''){
-    if(a != 0){
-        div.removeChild(list);
+    list.innerHTML = ' ';
+    tasks.forEach(task => {
+        let el = document.createElement('li');
+        el.textContent = task;
+        list.appendChild(el)
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    input.value = '';
+    
+}
+function addnew(){
+
+    let task = input.value;
+    if(task === '' || task.trim() === ''){return;}
+    else{
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        if(tasks===null){tasks = []}
+        tasks.push(task);
+        tasks.sort();
+        list.innerHTML = ' ';
+        tasks.forEach(task => {
+            let el = document.createElement('li');
+            el.textContent = task;
+            list.appendChild(el)
+        });
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        input.value = '';
+        
     }
-   list = document.createElement('ul');
-   del.push(input1.value);
-   del.sort();
-   div.appendChild(list)
-   for(let i = 0;i<del.length;i++){
-    el =  document.createElement('li');
-    el.innerHTML = del[i]
-    list.appendChild(el);
-    a++;
-   }
-   input1.value = null;
-  localStorage.setitem("tasks",list);
 }
+
+function Reset(){
+    localStorage.setItem('tasks',null);
+    list.innerHTML = ' ';
 }
+
+let ct = document.querySelector('#container');
+
+let  input = ct.appendChild(document.createElement('input'));
+
+let add = ct.appendChild(document.createElement('button'));
+add.textContent = 'Add';
+
+let reset = ct.appendChild(document.createElement('button'));
+reset.textContent = 'Reset';
+
+list = ct.appendChild(document.createElement('ul'));
+
+add.addEventListener('click', addnew);
+reset.addEventListener('click', Reset);
+
+restart();
